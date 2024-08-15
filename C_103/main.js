bdb083350317c3156773716ed20347514ff7dec3
ClashofClansPro1 <157911807+ClashofClansPro1@users.noreply.github.com>
2024-08-15 10:47:53 -0600

    Add files via upload

--- C_103/main.js
+++ C_103/main.js
@@ -13,4 +13,24 @@ function take_snapshot() {
     });
 }
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/RCffmrlg4/model.json" ,modelloded)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/RCffmrlg4/model.json" ,model_loaded);
+
+function model_loaded() {
+    console.log("el modelo esta listo");
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error,results){
+    if (error) {
+        console.error (error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
